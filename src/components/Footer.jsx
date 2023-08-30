d666fe2dd1f456d6d9ac617daa66d714b4218a44
Emmanuel <apotiolasegun@gmail.com>
2023-08-30 15:27:15 +0100

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Logo from "../img/cover.png"
+
+const Footer = () => {
+  return (
+    <footer>
+      <img src={Logo} alt=""/>
+      <span>
+        Made by <b>Ktech</b>
+      </span>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/context/authContext.jsx
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthContexProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(localStorage.getItem("user")) || null
+  );
+
+  const login = async (inputs) => {
+    
+      const res = await axios.post("http://localhost:8800/api/auth/login", inputs);
+      setCurrentUser(res.data);
+  
+   
+  };
+
+  const logout = async () => {
+    // Clear the localStorage
+    localStorage.removeItem("currentUser");
+    
+    // Clear the currentUser state
+    setCurrentUser(null);
+    
+    // Perform any additional logout-related actions
+    // For example: make a request to the backend to clear session
+  };
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(currentUser));
+  }, [currentUser]);
+
+  return (
+    <AuthContext.Provider value={{ currentUser, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
